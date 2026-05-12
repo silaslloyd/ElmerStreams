e8ee74391970963220224483df95ee3519a3c245
silaslloyd <silaspierslloyd1@gmail.com>
2026-05-12 09:12:22 +0200

    added mkdirs

--- read.cmd
+++ read.cmd
@@ -3,6 +3,12 @@
 #SBATCH --job-name="dataread"
 #SBATCH --ntasks=64
 #SBATCH --time=72:00:00
+cd OutputPlots
+mkdir -p T2
+cd T2
+mkdir -p sheet
+cd ../../
+
 nohup mpirun -np 64 python -u ReadResultFile.py "./footprint/InitT2" "./OutputPickles/T2" > "Initialisation/ScriptOutputs/dataT2.out"
 nohup python -u PlotResults.py "./OutputPickles/T2" "./OutputPlots/T2/" > ./"Initialisation/ScriptOutputs/plotT2.out"
 echo "DONE"
